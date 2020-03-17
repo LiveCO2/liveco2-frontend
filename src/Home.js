8bd19ebb9ab83fd51cc2a2bc8e4101ec2f18f766
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-03-17 10:59:00 +0100

    Allow different box models using CSS for inline box placement

--- src/Home.js
+++ src/Home.js
@@ -1,57 +1,43 @@
 import React from "react";
+import { Trans } from 'react-i18next';
  
 const Home = () => (
 	<div>
 		<div class="containerRow">
 			<div>
-				<h2 class="title">HELLO</h2>
-				<p>Cras facilisis urna ornare ex volutpat, et
-				convallis erat elementum. Ut aliquam, ipsum vitae
-				gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-				metus nec massa. Maecenas hendrerit laoreet augue
-				nec molestie. Cum sociis natoque penatibus et magnis
-				dis parturient montes, nascetur ridiculus mus.</p>
-
-				<p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+				<p><h2><Trans i18nKey="home.box1.title"/></h2></p>
+				<p><Trans i18nKey="home.box1.text1"/></p>
+				<p><Trans i18nKey="home.box1.text2"/></p>
+				<p><Trans i18nKey="home.box1.text3"/></p>
 			</div>
 		</div>
-		<div class="containerRow">
-			<div>
-				<h2 class="title">HELLO</h2>
-				<p>Cras facilisis urna ornare ex volutpat, et
-				convallis erat elementum. Ut aliquam, ipsum vitae
-				gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-				metus nec massa. Maecenas hendrerit laoreet augue
-				nec molestie. Cum sociis natoque penatibus et magnis
-				dis parturient montes, nascetur ridiculus mus.</p>
 
-				<p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+		<div class="containerRowFlexRightText">
+			<div class="rowFlexElement">
+				<p><h2><Trans i18nKey="home.box2.title"/></h2></p>
+				<p><Trans i18nKey="home.box2.text1"/></p>
+			</div>
+			<div class="rowFlexElementRightText">
+				<p><h2><Trans i18nKey="home.box2.title"/></h2></p>
+				<p><Trans i18nKey="home.box2.text1"/></p>
 			</div>
 		</div>
+
 		<div class="containerRow">
 			<div>
-				<h2 class="title">HELLO</h2>
-				<p>Cras facilisis urna ornare ex volutpat, et
-				convallis erat elementum. Ut aliquam, ipsum vitae
-				gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-				metus nec massa. Maecenas hendrerit laoreet augue
-				nec molestie. Cum sociis natoque penatibus et magnis
-				dis parturient montes, nascetur ridiculus mus.</p>
-
-				<p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+				<p><h2><Trans i18nKey="home.box3.title"/></h2></p>
+				<p><Trans i18nKey="home.box3.text1"/></p>
 			</div>
 		</div>
-		<div class="containerRow">
-			<div>
-				<h2 class="title">HELLO</h2>
-				<p>Cras facilisis urna ornare ex volutpat, et
-				convallis erat elementum. Ut aliquam, ipsum vitae
-				gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-				metus nec massa. Maecenas hendrerit laoreet augue
-				nec molestie. Cum sociis natoque penatibus et magnis
-				dis parturient montes, nascetur ridiculus mus.</p>
 
-				<p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+		<div class="containerRowFlexLeftText">
+			<div class="rowFlexElementLeftText">
+				<p><h2><Trans i18nKey="home.box2.title"/></h2></p>
+				<p><Trans i18nKey="home.box2.text1"/></p>
+			</div>
+			<div class="rowFlexElementLeftText">
+				<p><h2><Trans i18nKey="home.box2.title"/></h2></p>
+				<p><Trans i18nKey="home.box2.text1"/></p>
 			</div>
 		</div>
 	</div>
