567db6822224dae5e83083a6cc1e1197af07a119
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-04-05 16:25:28 +0200

    Display full viewport with a background image

--- src/Home.js
+++ src/Home.js
@@ -12,61 +12,63 @@ import iconTree3 from './icons/tree-3.svg'
 
 const Home = () => (
     <div>
+        <div className="containerRowCards">
+            <div className="card">
+                <h2 className="cardTitle"><Trans i18nKey="home.card1.title"/></h2>
+
+                <div className="cardListItem">
+                    <img src={iconCarrot} alt="An orange carrot" width="12%" className="icon"></img>
+                    <p className="cardListItemAmount"><Trans i18nKey="home.card1.item1.amount"/></p>
+                    <p className="cardListItemText"><Trans i18nKey="home.card1.item1.concept"/></p>
+                </div>
+                <div className="cardListItem">
+                    <img src={iconSolarPanel} alt="A solar panel" width="12%" className="icon"></img>
+                    <p className="cardListItemAmount"><Trans i18nKey="home.card1.item2.amount"/></p>
+                    <p className="cardListItemText"><Trans i18nKey="home.card1.item2.concept"/></p>
+                </div>
+                <div className="cardListItem">
+                    <img src={iconElectricCar} alt="An electric car" width="12%" className="icon"></img>
+                    <p className="cardListItemAmount"><Trans i18nKey="home.card1.item3.amount"/></p>
+                    <p className="cardListItemText"><Trans i18nKey="home.card1.item3.concept"/></p>
+                </div>
+                <div className="button">
+                    <a href="compensate" className="button"><Trans i18nKey="home.card1.button"/></a>
+                </div>
+            </div>
+            <div className="card">
+                <h2 className="cardTitle"><Trans i18nKey="home.card2.title"/></h2>
+
+                <div className="cardListItem">
+                    <img src={iconTree1} alt="A tree" width="12%" className="icon"></img>
+                    <p className="cardListItemAmount"><Trans i18nKey="home.card2.item1.amount"/></p>
+                    <p className="cardListItemText"><Trans i18nKey="home.card2.item1.concept"/></p>
+                </div>
+                <div className="cardListItem">
+                    <img src={iconTree2} alt="A tree" width="12%" className="icon"></img>
+                    <p className="cardListItemAmount"><Trans i18nKey="home.card2.item2.amount"/></p>
+                    <p className="cardListItemText"><Trans i18nKey="home.card2.item2.concept"/></p>
+                </div>
+                <div className="cardListItem">
+                    <img src={iconTree3} alt="A tree" width="12%" className="icon"></img>
+                    <p className="cardListItemAmount"><Trans i18nKey="home.card2.item3.amount"/></p>
+                    <p className="cardListItemText"><Trans i18nKey="home.card2.item3.concept"/></p>
+                </div>
+                <div className="button">
+                    <a href="how-it-works" className="button"><Trans i18nKey="home.card2.button"/></a>
+                </div>
+            </div>
+        </div>
+
         <div className="containerRow">
             <div className="containerRowElement">
                 <h2><Trans i18nKey="home.box1.title"/></h2>
                 <p><Trans i18nKey="home.box1.text1"/></p>
                 <p><Trans i18nKey="home.box1.text2"/></p>
                 <p><Trans i18nKey="home.box1.text3"/></p>
+                <a href="how-it-works" className="button"><Trans i18nKey="home.box1.button"/></a>
             </div>
             <div className="containerRowElement">
                 <img src={legoForest} alt="Lego Farmer" width="75%" className="image"></img>
-            </div>
-        </div>
-        <div className="containerRowCards">
-            <div className="rowCard">
-                <h2 className="rowCardTitle"><Trans i18nKey="home.card1.title"/></h2>
-
-                <div className="rowCardItem">
-                    <img src={iconCarrot} alt="An orange carrot" width="12%" className="icon"></img>
-                    <p className="rowCardItemAmount"><Trans i18nKey="home.card1.item1.amount"/></p>
-                    <p className="rowCardItemText"><Trans i18nKey="home.card1.item1.concept"/></p>
-                </div>
-                <div className="rowCardItem">
-                    <img src={iconSolarPanel} alt="A solar panel" width="12%" className="icon"></img>
-                    <p className="rowCardItemAmount"><Trans i18nKey="home.card1.item2.amount"/></p>
-                    <p className="rowCardItemText"><Trans i18nKey="home.card1.item2.concept"/></p>
-                </div>
-                <div className="rowCardItem">
-                    <img src={iconElectricCar} alt="An electric car" width="12%" className="icon"></img>
-                    <p className="rowCardItemAmount"><Trans i18nKey="home.card1.item3.amount"/></p>
-                    <p className="rowCardItemText"><Trans i18nKey="home.card1.item3.concept"/></p>
-                </div>
-                <div className="rowCardButton">
-                    <a href="compensate" className="rowCardButton"><Trans i18nKey="home.card1.button"/></a>
-                </div>
-            </div>
-            <div className="rowCard">
-                <h2 className="rowCardTitle"><Trans i18nKey="home.card2.title"/></h2>
-
-                <div className="rowCardItem">
-                    <img src={iconTree1} alt="A tree" width="12%" className="icon"></img>
-                    <p className="rowCardItemAmount"><Trans i18nKey="home.card2.item1.amount"/></p>
-                    <p className="rowCardItemText"><Trans i18nKey="home.card2.item1.concept"/></p>
-                </div>
-                <div className="rowCardItem">
-                    <img src={iconTree2} alt="A tree" width="12%" className="icon"></img>
-                    <p className="rowCardItemAmount"><Trans i18nKey="home.card2.item2.amount"/></p>
-                    <p className="rowCardItemText"><Trans i18nKey="home.card2.item2.concept"/></p>
-                </div>
-                <div className="rowCardItem">
-                    <img src={iconTree3} alt="A tree" width="12%" className="icon"></img>
-                    <p className="rowCardItemAmount"><Trans i18nKey="home.card2.item3.amount"/></p>
-                    <p className="rowCardItemText"><Trans i18nKey="home.card2.item3.concept"/></p>
-                </div>
-                <div className="rowCardButton">
-                    <a href="how-it-works" className="rowCardButton"><Trans i18nKey="home.card2.button"/></a>
-                </div>
             </div>
         </div>
 
