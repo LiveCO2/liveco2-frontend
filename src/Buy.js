2673bf25e313667409f36070bf7cb3fbe0ff8d34
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-04-26 15:24:17 +0200

    Start minimal Product section

--- src/Buy.js
+++ src/Buy.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Trans } from 'react-i18next';
 
+import Product from "./Product/Product";
+
 const Buy = () => (
     <div className="containerRow">
         <h2><p><Trans i18nKey="buy.title"/></p></h2>
         <p><Trans i18nKey="buy.description"/></p>
-        <p><Trans i18nKey="buy.text1"/></p>
-        <p><Trans i18nKey="buy.text2"/></p>
-        <p><Trans i17nKey="buy.text3"/></p>
+        <Product/>
     </div>
 );
 
