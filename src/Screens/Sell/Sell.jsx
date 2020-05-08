bbfdf2f4836927f733f325ecceeba32c64b5188c
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-05-08 19:02:01 +0200

    HowItWorks to Screens folder

--- src/Screens/Sell/Sell.jsx
+++ src/Screens/Sell/Sell.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Trans } from 'react-i18next';
 
+import "./Sell.scss"
+
 const Sell = () => (
     <div className="containerRow">
-        <h2><p><Trans i18nKey="sell.title"/></p></h2>
+        <h2 className="sellHeader"><Trans i18nKey="sell.title"/></h2>
         <p><Trans i18nKey="sell.description"/></p>
         <p><Trans i18nKey="sell.text1"/></p>
         <p><Trans i18nKey="sell.text2"/></p>
