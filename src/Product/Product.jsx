38de1f3243dd74576f868a151b5e473037e91375
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-05-08 21:57:22 +0200

    Enhance Products page

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -5,16 +5,16 @@ import Button from '../Button';
 
 import './Product.scss';
 
-const Product = ({img, title, desc, price}) => (
+const Product = ({img, title, desc, price, buttonText}) => (
   <div className="productContainer">
       <div className="productIcon">
           <img src={img} alt="Pack"></img>
       </div>
       <div className="productText">
           <h4 className="productTitle"><Trans i18nKey={title}/></h4>
-          <p><Trans i18nKey={desc}/></p>
+          <p className="productDescription"><Trans i18nKey={desc}/></p>
           <p className="productPrice"><Trans i18nKey={price}/></p>
-          <Button link="how-it-works" tk="home.box1.button" />
+          <Button link="how-it-works" tk={buttonText} />
       </div>
   </div>
 );
--- src/Screens/Buy/Buy.jsx
+++ src/Screens/Buy/Buy.jsx
@@ -10,7 +10,7 @@ import goldMedal from '../../icons/gold-medal.svg'
 import crown from '../../icons/crown.svg'
 
 const Buy = () => (
-    <div>
+    <div className="buy">
         <div className="buyHeader">
             <h2><Trans i18nKey="buy.title"/></h2>
             <p><Trans i18nKey="buy.description"/></p>
@@ -21,18 +21,21 @@ const Buy = () => (
                 title="buy.products.product1.title" 
                 desc="buy.products.product1.description"
                 price="buy.products.product1.price"
+                buttonText="buy.products.button"
             />
             <Product 
                 img={goldMedal}
                 title="buy.products.product2.title" 
                 desc="buy.products.product2.description"
                 price="buy.products.product2.price"
+                buttonText="buy.products.button"
             />
             <Product 
                 img={crown}
                 title="buy.products.product3.title" 
                 desc="buy.products.product3.description"
                 price="buy.products.product3.price"
+                buttonText="buy.products.button"
             />
         </div>
     </div>
