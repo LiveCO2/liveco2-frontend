cd57a7b291656609ef4e56c72ec33baf96679799
Eleonora Baret <eleonora.baret@gmail.com>
2020-05-08 18:59:01 +0200

    refactored Faq page

--- src/FaqBlock/FaqBlock.jsx
+++ src/FaqBlock/FaqBlock.jsx
@@ -1,46 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Trans } from 'react-i18next';
 
 import FaqItem from './../FaqItem/FaqItem';
 import { FaqBlockContainer, Title } from './styled';
 
-class FaqBlock extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          activeItemIndex: null,
-      };
-  }
-
-  handleItemClick = (index) => {
-      const { activeItemIndex } = this.state;
-      if (activeItemIndex === index) {
-          this.setState({ activeItemIndex: null });
-      } else {
-          this.setState({ activeItemIndex: index });
-      }
-  };
-  render() {
-    const { block, blockTitle } = this.props;
-    const { activeItemIndex } = this.state;
-
-    return (
-      <FaqBlockContainer>
-      <Title><Trans i18nKey={blockTitle}/></Title>
-        {block &&
-            block.map((item, index) => (
-                <FaqItem
-                    key={item.title}
-                    isActive={index === activeItemIndex}
-                    index={index}
-                    handleItemClick={this.handleItemClick}
-                    title={item.title}
-                    description={item.description}
-                />
-            ))}
-      </FaqBlockContainer>
-    );
-  };
+const FaqBlock = ({ block, blockTitle }) => {
+  const [activeStepIndex, setActiveItem] = useState({});
+  const handleItemClick = useCallback(
+    (index) => {
+        setActiveItem({
+            ...activeStepIndex,
+            [index]: !activeStepIndex[index],
+        });
+    },
+    [activeStepIndex],
+);
+  return (
+    <FaqBlockContainer>
+    <Title><Trans i18nKey={blockTitle}/></Title>
+      {block &&
+          block.map((item, index) => (
+              <FaqItem
+                  key={item.title}
+                  isActive={activeStepIndex[index] === true}
+                  handleItemClick={handleItemClick}
+                  index={index}
+                  title={item.title}
+                  description={item.description}
+              />
+          ))}
+    </FaqBlockContainer>
+  )
 };
 
 export default FaqBlock;
