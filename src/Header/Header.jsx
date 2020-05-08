0ac2107c5fcfbecf5829a9b357845199d6c252ac
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-05-08 12:54:41 +0200

    Remove section

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -10,7 +10,6 @@ const Header = () => (
     <div className="header">
         <div className="header-left">
             <NavLink className="header-link" exact to="/"><Trans i18nKey="header.home"/></NavLink>
-            <NavLink className="header-link" to="/how-it-works"><Trans i18nKey="header.how-it-works"/></NavLink>
             <NavLink className="header-link" to="/compensate"><Trans i18nKey="header.compensate"/></NavLink>
             <NavLink className="header-link" to="/enroll-trees"><Trans i18nKey="header.enroll-trees"/></NavLink>
             <NavLink className="header-link" to="/about-compensate"><Trans i18nKey="header.about"/></NavLink>
