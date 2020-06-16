60a3109c7ae694744ef7efcf41fcca0ecd1ca056
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-06-16 11:37:27 +0200

    Finish first version of signup and signin forms

--- src/User/Signup/Signup.jsx
+++ src/User/Signup/Signup.jsx
@@ -10,8 +10,10 @@ function Signup() {
         console.log(data);
     };
       return (
-          <div>
-              <div className="signup">
+          <div className="sign">
+              <div className="signBox">
+                <h2><Trans i18nKey="user.signup.title"/></h2>
+                <h5>Compensa tu huella de carbono. Compra Toneladas de CO2 absorbidas. Desbloquea contenido exclusivo</h5>
                 <form onSubmit={handleSubmit(onSubmit)} className="signupForm">
                     <div className="signupStep">
                         <label className="text-medium"><Trans i18nKey="user.signup.firstname"/></label>
@@ -54,28 +56,62 @@ function Signup() {
                                 validate: {
                                     has12Chars: value => value.length > 12,
                                     hasNumbers: value =>  /\d/.test(value),
-                                    hasSpecialChar: value => /[!@$%&()_+={};,.<>]/.test(value),
+                                    hasSpecialChar: value => /[!@$%&()_+={};,.]/.test(value),
                                 }})}
                         />
+                        <ul className="passwordRequirements">
+                            <li><p className="text-small"><Trans i18nKey="user.signup.pw-const-1"/></p></li>
+                            <li><p className="text-small"><Trans i18nKey="user.signup.pw-const-2"/></p></li>
+                            <li><p className="text-small"><Trans i18nKey="user.signup.pw-const-3"/></p></li>
+                        </ul>
                     </div>
 
                     {errors.firstname && (
-                        <p><Trans i18nKey="user.signup.errors.firstname"/></p>
+                        <p className="formError"><Trans i18nKey="user.signup.errors.firstname"/></p>
                     )}
                     {errors.lastname && (
-                        <p><Trans i18nKey="user.signup.errors.lastname"/></p>
+                        <p className="formError"><Trans i18nKey="user.signup.errors.lastname"/></p>
                     )}
                     {errors.email && (
-                        <p><Trans i18nKey="user.signup.errors.email"/></p>
+                        <p className="formError"><Trans i18nKey="user.signup.errors.email"/></p>
                     )}
                     {errors.password &&  (
-                        <p><Trans i18nKey="user.signup.errors.password"/></p>
+                        <p className="formError"><Trans i18nKey="user.signup.errors.password"/></p>
                     )}
-                  <input type="submit" />
+                    <div className="signupStep">
+                      <input type="submit" />
+                    </div>
+
+                    <div className="signupStep">
+                        <p className="text-tiny"><Trans i18nKey="user.signup.disclaimer"/></p>
+                    </div>
+
             </form>
          </div>
-         <div className="singin">
-             <h2><Trans i18nkey="user.signin.title"/></h2>
+
+         <div className="signBox">
+             <h2><Trans i18nKey="user.signin.title"/></h2>
+                <form onSubmit={handleSubmit(onSubmit)} className="signupForm">
+                    <div className="signupStep">
+                        <label className="text-medium"><Trans i18nKey="user.signup.email"/></label>
+                        <input 
+                            type="text"
+                            name="email"
+                            ref={register({required: true})}
+                        />
+                    </div>
+                    <div className="signupStep">
+                        <label className="text-medium"><Trans i18nKey="user.signin.password"/></label>
+                        <input 
+                            type="password"
+                            name="password"
+                            ref={register({required: true,})}
+                        />
+                    </div>
+                    <div className="signupStep">
+                      <input type="submit" />
+                    </div>
+            </form>
          </div>
      </div>
   );
